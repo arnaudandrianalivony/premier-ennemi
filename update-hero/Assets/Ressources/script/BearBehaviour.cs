caa51390c16bf84c9e8087718d5b6bf1417ac208
arnaudandrianalivony <arnaud.andrianalivony@epita.fr>
2019-04-20 22:50:57 +0200

    ajout d'un ennemi
    L'ennemi ne fait encore que détecter le joueur. On ne peut pas encore le tuer et lui non plus.

---
+++ update-hero/Assets/Ressources/script/BearBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearBehaviour : MonoBehaviour
+{
+    public Animator anim;                     // animation de l'ennemi
+    public Transform ennemyPos;               // position de l'ennemi
+    public Transform target;                  // position du joueur
+    public bool isInRange = false;            // definit si l'ennemi est a bonne distance du joueur
+    public float dist;                        // distance entre l'ennemi et le joueur
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.ResetTrigger("Attente");
+        dist = Vector2.Distance(ennemyPos.position, target.position);
+        isInRange = dist < 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector2.Distance(ennemyPos.position, target.position);
+        isInRange = dist < 10.0f;
+        if(isInRange)
+        {
+            anim.SetTrigger("Attente");
+        }
+        else
+        {
+            anim.ResetTrigger("Attente");
+        }
+    }
+}
---
+++ update-hero/Assets/Ressources/script/heroBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroBehaviour : MonoBehaviour
+{
+
+    public Rigidbody2D rb;                    // Rigidbody du hero
+    public float vitesse;                     // Vitesse du hero
+    public float upforce;                     // Distance de saut du hero
+    private Animator anim;                    // Animation du hero
+    public bool isfacing;                     // Definit si le hero est tourne vers la droite
+    private bool isGrounded;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        isfacing = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Run", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.zero;
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-vitesse, 0);
+            anim.SetBool("Run", true);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(vitesse, 0);
+            anim.SetBool("Run", true);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.velocity = Vector2.zero;
+            anim.SetBool("Run", false);
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rb.velocity = Vector2.zero;
+            anim.SetBool("Run", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(!(col.gameObject.CompareTag("Ground")))
+        {
+            isGrounded = false;
+        }
+    }
+
+    protected void Flip()
+    {
+        isfacing = !isfacing;
+        Vector3 vecteur = transform.localScale;
+        vecteur.x*=-1;
+        transform.localScale = vecteur;
+    }
+
+    
+}
