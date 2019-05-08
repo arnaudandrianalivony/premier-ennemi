3cb1280ca6d7c778922c0d4b962cc4cdc5154c5d
arnaudandrianalivony <arnaud.andrianalivony@epita.fr>
2019-05-08 13:43:47 +0200

    ajout d'un ennemi

---
+++ update_ennemi(1)/Assets/Ressources/script/EnnemyBehaviour.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyBehaviour : MonoBehaviour
+{
+    public Animator anim;                                    // represente l'annimation de l'ennemi;
+    public GameObject player;                                // represente le joueur
+    public float posPlayer;                                  // represente la position du joueur
+    public Collider2D colPlayer;                             // represente le collider du joueur
+    public float posEnnemy;                                  // represente la position de l'ennemi
+    public Collider2D colEnnemy;                             // represente le collider de l'ennemi
+    public float area = 15.0f;                               // zone definie pour l'ennemi
+    public float range;                                      // zone de vision de l'ennemi
+    public bool onGoing;                                     // defini si l'ennemi effectue un aller dans sons animation
+    public bool isInRange;                                   // defini si l'ennemi est a bonne distance du joueur
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posEnnemy = transform.position.x;
+        posPlayer = player.transform.position.x;
+        colPlayer = player.GetComponent<Collider2D>();
+        colEnnemy = GetComponent<Collider2D>();        
+        onGoing = true;
+        isInRange = false;
+        colPlayer = player.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posEnnemy = transform.position.x;
+        posPlayer = player.transform.position.x;
+        isInRange = (abs(posPlayer - posEnnemy) < range);
+        if(isInRange)
+        {
+            anim.SetTrigger("Run");
+            Follow();
+        }
+        else
+        {
+            if((posEnnemy < 0) || (posEnnemy > area))
+            {
+                onGoing = !onGoing;
+                Vector3 vect = transform.localScale;
+                vect.x*=-1;
+                transform.localScale = vect;
+            }
+            Walk();
+        }
+        if(colEnnemy.IsTouching(colPlayer))
+        {
+            anim.SetTrigger("Attack_ennemy");
+        }
+        
+    }
+
+    // fonction qui permet a l'ennemi d'effectuer des aller-retour
+    void Walk()
+    {
+        if(onGoing)
+        {
+            transform.position = new Vector3(transform.position.x + 0.1f, 0.0f, 0.0f);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x - 0.1f, 0.0f, 0.0f);
+        }
+    }
+
+    // fonction qui permet a l'ennemi de poursuivre le joueur
+    void Follow()
+    {
+        if(posPlayer + 0.1f < posEnnemy)
+        {
+            transform.position = new Vector3(transform.position.x - 0.2f, 0.0f, 0.0f);
+        }
+        else if(posPlayer - 0.1f > posEnnemy)
+        {
+            transform.position = new Vector3(transform.position.x + 0.2f, 0.0f, 0.0f);
+        }
+        else
+        {
+            transform.position = player.transform.position;
+            anim.ResetTrigger("Run");
+        }
+    }
+
+    // fonction valeur absolue
+    float abs(float a)
+    {
+        float res;
+        if(a > 0.0f)
+        {
+            res = a;
+        }
+        else
+        {
+            res = - a;
+        }
+        return res;
+    }
+}
