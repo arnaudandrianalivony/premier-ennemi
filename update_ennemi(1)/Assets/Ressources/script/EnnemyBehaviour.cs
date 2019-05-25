e129a42af1af4e3f6b1f51fd8b600f23b719fdd2
arnaudandrianalivony <arnaud.andrianalivony@epita.fr>
2019-05-25 11:48:22 +0200

    update_enemy
    amelioration de l'ennemi

--- update_ennemi(1)/Assets/Ressources/script/EnnemyBehaviour.cs
+++ update_ennemi(1)/Assets/Ressources/script/EnnemyBehaviour.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnnemyBehaviour : MonoBehaviour
 {
-    public Animator anim;                                    // represente l'annimation de l'ennemi;
-    public GameObject player;                                // represente le joueur
-    public float posPlayer;                                  // represente la position du joueur
-    public Collider2D colPlayer;                             // represente le collider du joueur
-    public float posEnnemy;                                  // represente la position de l'ennemi
-    public Collider2D colEnnemy;                             // represente le collider de l'ennemi
-    public float area = 15.0f;                               // zone definie pour l'ennemi
-    public float range;                                      // zone de vision de l'ennemi
-    public bool onGoing;                                     // defini si l'ennemi effectue un aller dans sons animation
-    public bool isInRange;                                   // defini si l'ennemi est a bonne distance du joueur
+    public Animator anim;                                      // represente l'annimation de l'ennemi;
+    public GameObject player;                                  // represente le joueur
+    public float posPlayer;                                    // represente la position du joueur
+    public Collider2D colPlayer;                               // represente le collider du joueur
+    public float posEnnemy;                                    // represente la position de l'ennemi
+    public Collider2D colEnnemy;                               // represente le collider de l'ennemi
+    public float area = 15.0f;                                 // zone definie pour l'ennemi
+    public float range;                                        // zone de vision de l'ennemi
+    public bool onGoing;                                       // defini si l'ennemi effectue un aller dans sons animation
+    public bool isInRange;                                     // defini si l'ennemi est a bonne distance du joueur
+    public float start;                                        // defini le point de depart de l'annimation de l'ennemi
+    public float end;                                          // defini le point d'arrivée de l'annimation de l'ennemi
 
 
     // Start is called before the first frame update
     void Start()
     {
         posEnnemy = transform.position.x;
+        start = posEnnemy;
+        end = posEnnemy + area;
         posPlayer = player.transform.position.x;
         colPlayer = player.GetComponent<Collider2D>();
         colEnnemy = GetComponent<Collider2D>();        
@@ -41,7 +46,7 @@ public class EnnemyBehaviour : MonoBehaviour
         }
         else
         {
-            if((posEnnemy < 0) || (posEnnemy > area))
+            if((posEnnemy < start) || (posEnnemy > end))
             {
                 onGoing = !onGoing;
                 Vector3 vect = transform.localScale;
@@ -53,6 +58,7 @@ public class EnnemyBehaviour : MonoBehaviour
         if(colEnnemy.IsTouching(colPlayer))
         {
             anim.SetTrigger("Attack_ennemy");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         
     }
@@ -75,11 +81,11 @@ public class EnnemyBehaviour : MonoBehaviour
     {
         if(posPlayer + 0.1f < posEnnemy)
         {
-            transform.position = new Vector3(transform.position.x - 0.2f, 0.0f, 0.0f);
+            transform.position = new Vector3(transform.position.x - 0.1f, 0.0f, 0.0f);
         }
         else if(posPlayer - 0.1f > posEnnemy)
         {
-            transform.position = new Vector3(transform.position.x + 0.2f, 0.0f, 0.0f);
+            transform.position = new Vector3(transform.position.x + 0.1f, 0.0f, 0.0f);
         }
         else
         {
@@ -87,6 +93,7 @@ public class EnnemyBehaviour : MonoBehaviour
             anim.ResetTrigger("Run");
         }
     }
+
 
     // fonction valeur absolue
     float abs(float a)
